4b2ee37fdebd4dd4e2307d1a57ed7f72cb1fd61b
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-06-17 11:33:19 +0200

    showing the abilities, and base form and 2 evolutions

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if (isset($_POST['SearchPokemon'])) {
     $input = $_POST['searchInput'];
     $input = strtolower($input);
-    var_dump($input);
+    //var_dump($input);
     $url = "https://pokeapi.co/api/v2/pokemon/$input";
     $pokemonFetch = file_get_contents($url);
     $pokemon = json_decode($pokemonFetch, true);
@@ -10,6 +10,7 @@ if (isset($_POST['SearchPokemon'])) {
     $pokeName = $pokemon['name'];
     $frontSprite = $pokemon['sprites']['front_default'];
     $pokemonID = $pokemon['id'];
+    $pokemonAbilities = $pokemon['abilities']['0']['ability']['name'];
 
     if (count($pokemon['moves']) > 1) {
         $move1 = $pokemon['moves']['0']['move']['name'];
@@ -23,27 +24,36 @@ if (isset($_POST['SearchPokemon'])) {
     $speciesUrl = $pokemon['species']['url'];
     $fetchSpecies = file_get_contents($speciesUrl);
     $speciesData = json_decode($fetchSpecies, true);
-    var_dump($pokemon['species']['url']);
+    //var_dump($pokemon['species']['url']);
 
     $evoFetchChainUrl = $speciesData['evolution_chain']['url'];
     $evoFetchChain = file_get_contents($evoFetchChainUrl);
     $evoChainData = json_decode($evoFetchChain, true);
 
-    $baseFormUrl = $evoChainData['chain']['species']['url'];
-    $evoName = $evoChainData['chain']['evolves_to'][0]['species']['url'];
-    $evoName2 = $evoChainData['chain']['evolves_to'][0]['evolves_to'][0]['species']['url'];
+    $baseFormName = $evoChainData['chain']['species']['name'];
+    if (count($evoChainData['chain']['evolves_to']) > 0) {
+        $evoName = $evoChainData['chain']['evolves_to'][0]['species']['name'];
+        if (count($evoChainData['chain']['evolves_to'][0]['evolves_to']) > 0) {
+            $evoName2 = $evoChainData['chain']['evolves_to'][0]['evolves_to'][0]['species']['name'];
+        } else $evoName2 = "";
+    } else $evoName = "";
 
-    $baseFormFetch = file_get_contents($baseFormUrl);
+    $baseFormFetch = file_get_contents("https://pokeapi.co/api/v2/pokemon/$baseFormName");
     $baseFormData = json_decode($baseFormFetch, true);
+    $baseFormSprite = $baseFormData['sprites']['other']['official-artwork']['front_default'];
 
-    $evoFetch = file_get_contents($evoName);
-    $evoNameData = json_decode($evoFetch);
+    $evoFetch = file_get_contents("https://pokeapi.co/api/v2/pokemon/$evoName");
+    $evoNameData = json_decode($evoFetch, true);
+    if ($evoName) {
+        $firstEvoSprite = $evoNameData['sprites']['other']['official-artwork']['front_default'];
+        if ($evoName2) {
+            $secondEvoFetch = file_get_contents("https://pokeapi.co/api/v2/pokemon/$evoName2");
+            $secondEvoData = json_decode($secondEvoFetch, true);
+            $secondEvoSprite = $secondEvoData['sprites']['other']['official-artwork']['front_default'];
+        } else $secondEvoSprite = "";
+    } else $firstEvoSprite = "";
 
-    $secondEvoFetch = file_get_contents($evoName2);
-    $secondEvoData = json_decode($secondEvoFetch);
-
-
-
+    //var_dump($baseFormData);
     //var_dump($pokeName);
     //var_dump($frontSprite);
 }
@@ -61,11 +71,10 @@ if (isset($_POST['SearchPokemon'])) {
 <body>
 
 <div class="pokedexContainer">
-    //title
+
     <img src="Resources/Capture-removebg-preview.png" alt=""/>
 
 
-    //Search Box
     <form method="post">
         <input id="SearchInput" type="text" name="searchInput">
         <input type="submit" value="Search Pokemon" name="SearchPokemon">
@@ -73,35 +82,60 @@ if (isset($_POST['SearchPokemon'])) {
     </form>
 
 
-    //Pokemon Card
     <div class="pokemonCard" id="pokemonInfo">
         <p>Name: <?php
-        if (isset($_POST['SearchPokemon'])) {
-            echo $pokeName;
-        } ?> </p>
+            if (isset($_POST['SearchPokemon'])) {
+                echo $pokeName;
+            } ?> </p>
         <p>ID: <?php
             if (isset($_POST['SearchPokemon'])) {
                 echo $pokemonID;
             } ?> </p>
         <p>Moves: <?php
             if (isset($_POST['SearchPokemon'])) {
-                echo implode(" ",$moves);
+                echo implode(", ", $moves);
+            } ?></p>
+        <p>Ability: <?php
+            if (isset($_POST['SearchPokemon'])) {
+            echo $pokemonAbilities;
             } ?></p>
         <img src="
                 <?php
         if (isset($_POST['SearchPokemon'])) {
-                echo $frontSprite;
-            } ?>" alt="" id="frontSprite">
+            echo $frontSprite;
+        } ?>" alt="" id="frontSprite">
 
     </div>
 
-    //Pokemon Evolutions
+
     <div class="pokemonEvos">
-        <p>Base form: </p>
-        <p>First evolution: </p>
-        <p>Second Evolution: </p>
+        <p>Base form:
+            <img src="
+                <?php
+            if (isset($_POST['SearchPokemon'])) {
+                echo $baseFormSprite;
+            } ?>" alt="">
+        <p>First evolution:
+            <img src="
+                <?php
+            if (isset($_POST['SearchPokemon'])) {
+                if ($firstEvoSprite) {
+                    echo $firstEvoSprite;
+                } else echo "Resources/pokemon-ball-icon-9.jpg";
+            } ?>" alt=""></p>
+        <p>Second Evolution:
+            <img src="
+            <?php
+            if (isset($_POST['SearchPokemon'])) {
+                if ($firstEvoSprite) {
+                    if ($secondEvoSprite) {
+                        echo $secondEvoSprite;
+                    } else echo "Resources/pokemon-ball-icon-9.jpg";
+                } else echo "Resources/pokemon-ball-icon-9.jpg";
+            } ?>" alt=""></p>
     </div>
 </div>
+
 
 </body>
 </html>
